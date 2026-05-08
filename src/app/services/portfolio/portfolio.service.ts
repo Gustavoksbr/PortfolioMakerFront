242ed87e818219e9b933ae99ff1db96ca76af498
Gustavoksbr <gustavosalesi@hotmail.com>
2026-05-08 18:55:41 -0300

    refactor: update listar() to use ?without=gustavoksbr query param

--- src/app/services/portfolio/portfolio.service.ts
+++ src/app/services/portfolio/portfolio.service.ts
@@ -1,12 +1,12 @@
-﻿import {Injectable} from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Portfolio} from '../../models/response/Portfolio';
-import {AuthService} from '../autenticacao/auth.service';
-import {PortfolioRequest} from '../../models/request/PortfolioRequest';
-import {gustavoksbr} from '../../backup/db';
-import {API_CONFIG} from '../config/api.config';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Portfolio } from '../../models/response/Portfolio';
+import { AuthService } from '../autenticacao/auth.service';
+import { PortfolioRequest } from '../../models/request/PortfolioRequest';
+import { gustavoksbr } from '../../backup/db';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
@@ -18,39 +18,38 @@ export class PortfolioService {
   constructor(private http: HttpClient, private authService: AuthService) {
   }
   private getHeaders(): HttpHeaders {
-    return  this.authService.getHeaders();
+    return this.authService.getHeaders();
   }
-  listar() : Observable<Portfolio[]>{
-    //console.log("fazendo http get em "+this.API);
-    return this.http.get<Portfolio[]>(this.API + "/no-gustavoksbr");
+  listar(): Observable<Portfolio[]> {
+    return this.http.get<Portfolio[]>(this.API + "?without=gustavoksbr");
   }
 
-  listarTodos() : Observable<Portfolio[]>{
+  listarTodos(): Observable<Portfolio[]> {
     return this.http.get<Portfolio[]>(this.API);
   }
-// http://localhost:8080/portfolio/get/username/teste9
-  mostrarPortfolioPorUsername(username: string) : Observable<Portfolio>{
-    if(username=="gustavoksbr"){
+  // http://localhost:8080/portfolio/get/username/teste9
+  mostrarPortfolioPorUsername(username: string): Observable<Portfolio> {
+    if (username == "gustavoksbr") {
       return new Observable(observer => {
         observer.next(this.gu);
         observer.complete();
       });
     }
-    return this.http.get<Portfolio>(this.API+"/username/"+username);
+    return this.http.get<Portfolio>(this.API + "/username/" + username);
   }
-  mostrarPortfolioPorEmail(email: string) : Observable<Portfolio>{
-    if(email=="gustavosalesi@hotmail.com"){
+  mostrarPortfolioPorEmail(email: string): Observable<Portfolio> {
+    if (email == "gustavosalesi@hotmail.com") {
       return new Observable(observer => {
         observer.next(this.gu);
         observer.complete();
       });
     }
-    return this.http.get<Portfolio>(this.API+"/email/"+email);
+    return this.http.get<Portfolio>(this.API + "/email/" + email);
   }
 
   savePortfolio(portfolio: PortfolioRequest): Observable<Portfolio> {
     //console.log("portfolioRequest",portfolio);
-    return this.http.post<Portfolio>(this.API+"/save",portfolio,{ headers: this.getHeaders() } );
+    return this.http.post<Portfolio>(this.API + "/save", portfolio, { headers: this.getHeaders() });
   }
 
 
